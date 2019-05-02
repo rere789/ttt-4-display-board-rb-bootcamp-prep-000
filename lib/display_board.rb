5ace3f5ba33878ea2dd8166bff21eb9697ed7068
Reina Ewing <reina.castro3@gmail.com>
2019-05-02 15:15:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " "|" "|" "
   puts -----------
+  puts " "|" "|" "
+  puts -----------
+  puts " "|" "|" "
 end 
 
 puts display_board(board)
